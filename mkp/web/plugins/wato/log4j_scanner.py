a63b8fe0bd477bfa98f31ca35b1b1079f4d05558
Maximilian Hill <mhill@inett.de>
2021-12-15 20:16:10 +0100

    allow extension without bakery

--- mkp/web/plugins/wato/log4j_scanner.py
+++ mkp/web/plugins/wato/log4j_scanner.py
@@ -11,41 +11,43 @@ from cmk.gui.plugins.wato import (
     HostRulespec,
     rulespec_registry,
 )
-from cmk.gui.cee.plugins.wato.agent_bakery.rulespecs.utils import (
-    RulespecGroupMonitoringAgentsAgentPlugins
-)
 from cmk.gui.valuespec import (
     Alternative,
     FixedValue,
-)
+)import cmk.utils.version as cmk_version
 
 
-def _valuespec_agent_config_log4j_scanner():
-    return Alternative(
-        title=_("log4j CVE-2021-44228 (Linux)"),
-        help=_(
-            "scan for CVE-2021-44228 (<tt>log4j_scanner</tt>)<br/>"
-            "<b>This plugin may result in massive agent outputs</b>"
-            ),
-        style='dropdown',
-        elements=[
-            FixedValue(
-                True,
-                title=_("Deploy plugin to scan for log4j"),
-                totext=_("(enabled)"),
-            ),
-            FixedValue(
-                None,
-                title=_("Do not deploy plugin to scan for log4j"),
-                totext=_("(disabled)"),
-            )
-        ],
+if cmk_version.is_enterprise_version() or cmk_version.is_managed_version():
+    from cmk.gui.cee.plugins.wato.agent_bakery.rulespecs.utils import (
+        RulespecGroupMonitoringAgentsAgentPlugins
     )
 
+    def _valuespec_agent_config_log4j_scanner():
+        return Alternative(
+            title=_("log4j CVE-2021-44228 (Linux)"),
+            help=_(
+                "scan for CVE-2021-44228 (<tt>log4j_scanner</tt>)<br/>"
+                "<b>This plugin may result in massive agent outputs</b>"
+                ),
+            style='dropdown',
+            elements=[
+                FixedValue(
+                    True,
+                    title=_("Deploy plugin to scan for log4j"),
+                    totext=_("(enabled)"),
+                ),
+                FixedValue(
+                    None,
+                    title=_("Do not deploy plugin to scan for log4j"),
+                    totext=_("(disabled)"),
+                )
+            ],
+        )
 
-rulespec_registry.register(
-    HostRulespec(
-        group=RulespecGroupMonitoringAgentsAgentPlugins,
-        name="agent_config:log4j_scanner",
-        valuespec=_valuespec_agent_config_log4j_scanner,
-    ))
+
+    rulespec_registry.register(
+        HostRulespec(
+            group=RulespecGroupMonitoringAgentsAgentPlugins,
+            name="agent_config:log4j_scanner",
+            valuespec=_valuespec_agent_config_log4j_scanner,
+        ))
