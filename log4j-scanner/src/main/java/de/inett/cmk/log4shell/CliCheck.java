8bd4f485e1f57f4df8800f3c0e0415c1332b2e7a
Maximilian Hill <mhill@inett.de>
2021-12-18 17:15:44 +0100

    scan for log4j1

--- log4j-scanner/src/main/java/de/inett/cmk/log4shell/CliCheck.java
+++ log4j-scanner/src/main/java/de/inett/cmk/log4shell/CliCheck.java
@@ -2,18 +2,25 @@ package de.inett.cmk.log4shell;
 
 import com.logpresso.scanner.Log4j2Scanner;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CliCheck {
-    public static void main(String[] args) {
+    public static void main(String[] cargs) {
         String[] n_args;
-        switch (args.length) {
+        switch (cargs.length) {
             case 0:
-                n_args = new String[]{"/"};
+                n_args = new String[]{"/", "--scan-log4j1"};
                 break;
             case 1:
-                n_args = new String[]{args[0]};
+                n_args = new String[]{cargs[0], "--scan-log4j1"};
                 break;
             default:
-                n_args = args;
+                List<String> t_args = Arrays.asList(cargs);
+                if (! t_args.contains("--scan-log4j1")) {
+                    t_args.add("--scan-log4j1");
+                }
+                n_args = t_args.toArray(new String[0]);
                 break;
         }
         System.out.println("<<<log4j_scanner>>>");
