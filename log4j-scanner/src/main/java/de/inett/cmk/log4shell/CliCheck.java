a50fa4f4293d2f9eee2d29e3b1a60a37747840f9
Maximilian Hill <mhill@inett.de>
2021-12-19 00:03:39 +0100

    Scan for logback (CVE-2021-42550)

--- log4j-scanner/src/main/java/de/inett/cmk/log4shell/CliCheck.java
+++ log4j-scanner/src/main/java/de/inett/cmk/log4shell/CliCheck.java
@@ -2,29 +2,40 @@ package de.inett.cmk.log4shell;
 
 import com.logpresso.scanner.Log4j2Scanner;
 
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class CliCheck {
     public static void main(String[] cargs) {
-        String[] n_args;
-        switch (cargs.length) {
-            case 0:
-                n_args = new String[]{"/", "--scan-log4j1"};
-                break;
-            case 1:
-                n_args = new String[]{cargs[0], "--scan-log4j1"};
-                break;
-            default:
-                List<String> t_args = Arrays.asList(cargs);
-                if (! t_args.contains("--scan-log4j1")) {
-                    t_args.add("--scan-log4j1");
-                }
-                n_args = t_args.toArray(new String[0]);
-                break;
-        }
         System.out.println("<<<log4j_scanner>>>");
+        String[] n_args;
         try {
+            switch (cargs.length) {
+                case 0:
+                    n_args = new String[]{
+                            "--scan-log4j1", "--scan-logback", "/"
+                    };
+                    break;
+                case 1:
+                    n_args = new String[]{
+                            "--scan-log4j1", "--scan-logback", cargs[0]
+                    };
+                    break;
+                default:
+                    List<String> t_args = new LinkedList<>();
+                    t_args.add("--scan-log4j1");
+                    t_args.add("--scan-logback");
+                    for (String arg : cargs) {
+                        if ( ! (
+                                arg.equals("--scan-log4j1")
+                                || arg.equals("--scan-logback")
+                        )) {
+                            t_args.add(arg);
+                        }
+                    }
+                    n_args = t_args.toArray(new String[0]);
+                    break;
+            }
             Log4j2Scanner scanner = new Log4j2Scanner();
             scanner.run(n_args);
         } catch (Throwable t) {
