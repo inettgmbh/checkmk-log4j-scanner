d34d54b7b9496af047fabe4b1a5af389677afef1
Maximilian Hill <mhill@inett.de>
2021-12-20 12:57:21 +0100

    don't exit

--- log4j-scanner/src/main/java/de/inett/cmk/log4shell/CliCheck.java
+++ log4j-scanner/src/main/java/de/inett/cmk/log4shell/CliCheck.java
@@ -1,46 +1,64 @@
 package de.inett.cmk.log4shell;
 
+import com.logpresso.scanner.Configuration;
 import com.logpresso.scanner.Log4j2Scanner;
+import com.logpresso.scanner.Metrics;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class CliCheck {
-    public static void main(String[] cargs) {
+public class CliCheck extends Log4j2Scanner {
+
+    Configuration config;
+    Metrics metrics;
+
+    public static void main(String[] args) {
         System.out.println("<<<log4j_scanner>>>");
-        String[] n_args;
+        String[] n_args = prepareArguments(args);
         try {
-            switch (cargs.length) {
-                case 0:
-                    n_args = new String[]{
-                            "--scan-zip", "--scan-logback", "/"
-                    };
-                    break;
-                case 1:
-                    n_args = new String[]{
-                            "--scan-log4j1", "--scan-logback", cargs[0]
-                    };
-                    break;
-                default:
-                    List<String> t_args = new LinkedList<>();
-                    t_args.add("--scan-zip");
-                    t_args.add("--scan-logback");
-                    for (String arg : cargs) {
-                        if ( ! (
-                                arg.equals("--scan-zip")
-                                || arg.equals("--scan-logback")
-                        )) {
-                            t_args.add(arg);
-                        }
-                    }
-                    n_args = t_args.toArray(new String[0]);
-                    break;
-            }
-            Log4j2Scanner scanner = new Log4j2Scanner();
+            Log4j2Scanner scanner = new CliCheck();
             scanner.run(n_args);
         } catch (Throwable t) {
             System.out.println("Error: " + t.getMessage());
         }
         System.exit(0);
+    }
+
+    @Override
+    public void run(String[] args) throws Exception {
+        config = Configuration.parseArguments(args);
+		metrics = new Metrics();
+
+        run();
+    }
+    private static String[] prepareArguments(String[] cargs) {
+        String[] ret;
+        switch (cargs.length) {
+            case 0:
+                ret = new String[]{
+                        "--scan-zip", "--scan-logback", "/"
+                };
+                break;
+            case 1:
+                ret = new String[]{
+                        "--scan-log4j1", "--scan-logback", cargs[0]
+                };
+                break;
+            default:
+                List<String> t_args = new LinkedList<>();
+                t_args.add("--scan-zip");
+                t_args.add("--scan-logback");
+                for (String arg : cargs) {
+                    if ( ! (
+                            arg.equals("--scan-zip")
+                                    || arg.equals("--scan-logback")
+                    )) {
+                        t_args.add(arg);
+                    }
+                }
+                ret = t_args.toArray(new String[0]);
+                break;
+        }
+        return ret;
     }
 }
