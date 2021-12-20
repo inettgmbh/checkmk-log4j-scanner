e6ab7107883a23b24b63380b533f4f3948727fce
Maximilian Hill <mhill@inett.de>
2021-12-20 19:00:14 +0100

    Separate services for vulnerabilities

--- mkp/agent_based/log4j_scanner.py
+++ mkp/agent_based/log4j_scanner.py
@@ -6,6 +6,11 @@
 # A file is subject to the terms and conditions defined in the file LICENSE,
 # which is part of this source code package.
 
+from .agent_based_api.v1.type_defs import (
+    CheckResult,
+    DiscoveryResult,
+)
+
 from .agent_based_api.v1 import (
     register,
     Service,
@@ -16,23 +21,35 @@ import collections
 
 
 def log4j_scanner_discovery(section):
-    yield Service()
+    def vulns():
+        yield {"cve": "CVE-2021-44228", "product": "log4j 2"}
+        yield {"cve": "CVE-2021-45046", "product": "log4j 2.15.0"}
+        yield {"cve": "CVE-2021-45105", "product": "log4j 2.16.0"}
+        yield {"cve": "CVE-2021-4104", "product": "log4j 1.x"}
+        yield {"cve": "CVE-2021-42550", "product": "logback 0.9-1.2.7"}
+
+    for vuln in vulns():
+        yield Service(
+            item=("%s (%s)" % (vuln.get("cve"), vuln.get("product"))),
+            parameters=vuln
+        )
 
 
-def log4j_scanner_checks(section):
+def log4j_scanner_checks(item, params, section):
+    cve, product = params["cve"].lower(), params["product"]
     f, p, e = 0, 0, 0
-    eld = False
-    lb = collections.deque(maxlen=6)
+    empty_line_detected = False
+    endblock = collections.deque(maxlen=6)
     for line in section:
-        if eld:
-            lb.append(line)
+        if empty_line_detected:
+            endblock.append(line)
         if len(line) == 0:
-            eld = True
+            empty_line_detected = True
             continue
-        if line[0] == "[*]":
+        if line[0] == "[*]" and line[2].lower() == cve:
             f = (f + 1)
             yield Result(state=State.CRIT, summary=(' '.join(line[2:])))
-        elif line[0] == "[?]":
+        elif line[0] == "[?]" and line[2].lower() == cve:
             p = (p + 1)
             yield Result(
                 state=State.CRIT,
@@ -40,10 +57,16 @@ def log4j_scanner_checks(section):
             )
         elif ' '.join(line[0:1]) == "Scan error:":
             e = (e + 1)
-    for line in lb:
+
+    for line in endblock:
         if len(line) == 6 and line[0] == "Scanned" and line[2] == \
                 "directories" and line[5] == "files":
-            yield Result
+            yield Result(
+                state=State.OK, summary=("%d files scanned" % line[4])
+            )
+            yield Result(
+                state=State.OK, summary=("%d directories scanned" % line[1])
+            )
     f_state = (State.OK if (f == 0) else State.CRIT)
     yield Result(state=f_state, summary=("%d vulnerabilities found" % f))
     p_state = (State.OK if (p == 0) else State.CRIT)
@@ -61,7 +84,9 @@ register.agent_section(
 
 register.check_plugin(
     name="log4j_scanner",
-    service_name="Scan for log4j CVE-2021-44228",
+    service_name="Vulnerability %s",
+    sections=["log4j_scanner"],
     discovery_function=log4j_scanner_discovery,
     check_function=log4j_scanner_checks,
+    check_default_parameters={},
 )
